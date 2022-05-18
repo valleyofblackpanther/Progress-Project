ab3b7946593610039bbb089b5dcb5ea344e6ca63
Kaushik Chintam <64037947+valleyofblackpanther@users.noreply.github.com>
2022-05-18 23:30:38 +0530

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,11 @@
+countries = ['United Kingdom', 'india', 'bangladesh']
+print(countries)
+if(countries == 'india'):
+ for each in countries:
+     print(each)
+if(each == 'United Kingdom'):
+     print("It was uk")
+
+wassup = [[['uk','inida','bangladesh'],'us','pak','mal'],'jap','nk','sr']
+wassup[0] = 'united states'
+print(wassup[1[0]])
